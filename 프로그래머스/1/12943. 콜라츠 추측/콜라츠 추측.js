318addf276ddcb0a831439a17bc53895db41f05e
Yejin Ahn <79955539+yyejin00@users.noreply.github.com>
2025-12-30 13:28:39 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,12 @@
+function solution(num) {
+    let result = 0;
+    
+    while(num!==1){
+        num%2===0 ? num = num/2 : num = num*3+1;
+        ++result;
+        if (result>500) return -1;
+        
+    }
+    
+    return result;
+}
